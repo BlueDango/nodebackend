58d0583982d41d8f253981eb268619c783b8593b
BlueDango <thebluedango@gmail.com>
2015-05-13 01:01:50 -0700

    initial commit

---
+++ njs/njs/modules/Router.ts
@@ -0,0 +1,63 @@
+﻿import express = require('express');
+import mongodb = require('mongodb');
+
+var MyMongoClient = mongodb.MongoClient;
+var mongoUrl: string = 'mongodb://localhost:27017/test';
+
+export class Router {
+    showData(res: express.Response) {
+        MyMongoClient.connect(mongoUrl, function (err, db) {
+            _getAllMessages(db, function (entries) {
+                db.close();
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(entries));
+            });
+        });
+    }
+
+    submitData(s: string, res: express.Response) {
+        MyMongoClient.connect(mongoUrl, function (err, db) {
+            _insertMessage(db, s, function () {
+                db.close();
+                res.end(s + " was submitted");
+            });
+        });
+    }
+
+    clearData(res: express.Response) {
+        MyMongoClient.connect(mongoUrl, function (err, db) {
+            _removeAllMessages(db, function () {
+                db.close();
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end("Data cleared!");
+            });
+        });
+    }
+};
+
+function _getAllMessages(db: mongodb.Db, callback: Function) {
+    var cursor = db.collection('messages').find();
+    var entry = [];
+    cursor.each(function (err, doc) {
+        if (doc != null) {
+            entry.push(doc);
+        } else {
+            callback(entry);
+        }
+    });
+};
+
+function _insertMessage(db, entry: String, callback: Function) {
+    db.collection('messages').insertOne(entry, function (err, result) {
+        console.log("Inserted " + entry);
+        callback(result);
+    });
+};
+
+function _removeAllMessages(db, callback) {
+    db.collection('messages').deleteMany({},
+        function (err, results) {
+            console.log(results);
+            callback();
+        });
+}
---
+++ njs/njs/static/js/myTest.ts
@@ -0,0 +1,18 @@
+﻿function sendRequest(requestType: string, url: string, postData: string) {
+    var oReq: XMLHttpRequest = new XMLHttpRequest();
+    oReq.onload = reqListener;
+    oReq.open(requestType, url, true);
+    oReq.send(postData);
+}
+
+function reqListener(): void {
+    var childNode: HTMLElement = document.getElementById("msgNode");
+    var p: HTMLElement = document.createElement("p");
+    p.id = "msgNode";
+    p.innerHTML = this.responseText;
+    if (childNode) {
+        document.getElementById("msgArea").replaceChild(p, childNode);
+    } else {
+        document.getElementById("msgArea").appendChild(p);
+    }
+}
